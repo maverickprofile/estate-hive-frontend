bf23c3678378892720afa7dd4357d62f9f03f848
Mahesh Kumar Kamalakar <71766922+maverickprofile@users.noreply.github.com>
2025-08-09 13:55:42 +0530

    Load property listings from Supabase

--- src/util/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://mtjxfyzcuuvtplemliwe.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10anhmeXpjdXV2dHBsZW1saXdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyNzU3NTYsImV4cCI6MjA2ODg1MTc1Nn0.KxsHhDcmJIDcPncNXgL05QvDvOm20l0t0vTQdSF0qPg';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
